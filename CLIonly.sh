4048665769bb3518b41a571b71bdf424c2f1b0d9
novaTopFlex <andrewkaymars@proton.me>
2024-05-15 14:49:47 -0400

    Update CLIonly.sh

--- CLIonly.sh
+++ CLIonly.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-DIALOG=$(dialog --erase-on-exit --stdout --menu "Select Application/Command..." 0 0 0\
-1 "ls"\
-2 "ls -l"\
-3 "ls -a"\
-4 "ls -al"\
-5 "ip a"\
-6 "ss"\
-7 "sudo tshark"\
-8 "ps aux"\
-9 "ps -ely"\
-10 "cal"\
-11 "ncal"\
+DIALOG=$(dialog --erase-on-exit --stdout --menu "Select Application/Command..." 0 0 0 \
+1 "ls" \
+2 "ls -l" \
+3 "ls -a" \
+4 "ls -al" \
+5 "ip a" \
+6 "ss" \
+7 "sudo tshark" \
+8 "ps aux" \
+9 "ps -ely" \
+10 "cal" \
+11 "ncal" \
 12 "date")
 if [[ $DIALOG == 1 ]]
 then ls
