b24df41f88a5401e0adecd510f4e32f47ba07f7b
novaTopFlex <andrewkaymars@proton.me>
2024-05-15 14:39:51 -0400

    Update CLIonly.sh

--- CLIonly.sh
+++ CLIonly.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 DIALOG=$(dialog --erase-on-exit --stdout --menu "Select Application..." 0 0 0 1 "ls" 2 "ls -l" 3 "ls -a" 4 "ls -al")
-if [ $DIALOG == 1 ]
+if [[ $DIALOG == 1 ]]
 then ls
-elif [ $DIALOG == 2 ]
+elif [[ $DIALOG == 2 ]]
 then ls -l
-elif [ $DIALOG == 3 ]
+elif [[ $DIALOG == 3 ]]
 then ls -a
-elif [ $DIALOG == 4 ]
+elif [[ $DIALOG == 4 ]]
 then ls -al
 else
 echo "Not Available."
